156a794563914f2120b7ab2731cbde220812e83c
Anandnaprabhu S <sp.anandhaprabhu@gmail.com>
2017-05-17 14:08:18 +0530

    Add files via upload

---
+++ Advertise/api/read_all_ads.php
@@ -0,0 +1,23 @@
+<?php
+// include core configuration
+include_once '../config/core.php';
+ 
+// include database connection
+include_once '../config/database.php';
+ 
+// product object
+include_once '../objects/product.php';
+ 
+// class instance
+$database = new Database();
+$db = $database->getConnection();
+$product = new Product($db);
+ 
+// read all products
+$product->category=$_GET['category'];
+$product->location=$_GET['location'];
+$results=$product->readAds();
+ 
+// output in json format
+echo $results;
+?>

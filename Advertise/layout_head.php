5b33b7095e32411345a3cb6e4ebdae5b64d23c11
Anandnaprabhu S <sp.anandhaprabhu@gmail.com>
2017-05-17 14:37:15 +0530

    Add files via upload

---
+++ Advertise/layout_head.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <title>Advertise</title>
+ 
+    <!-- Bootstrap CSS -->
+    <link href="libs/css/bootstrap/dist/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="libs/css/styles.css" rel="stylesheet">
+    <link href="libs/css/aos.css" rel="stylesheet">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="libs/animate.css" />
+    <link rel="stylesheet" type="text/css" href="libs/demo.css" />
+    <script type="text/javascript">
+        
+    </script>
+</head>
+<body>
+ <div id="fb-root"></div>
+<!-- <script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=211255495983573";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script> -->
+
+    <div>
